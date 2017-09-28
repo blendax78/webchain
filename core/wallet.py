9ea42ba026cadcde4994449ef84f595849e5bf27
Eric Vintimilla <eric@vintimilla.org>
2017-09-28 10:57:27 -0400

    add create() function

--- core/wallet.py
+++ core/wallet.py
@@ -74,6 +74,56 @@ class wallet(object):
 
   #   return result
 
+
+# test1() vs test()
+# need to compress pub key
+# {'secret_key_58': '5HwfZDuZazMEaYRQ5va85xEtpgWkncPZ54XZKMYfJtLi6paZb55', 'public_key': '(87683569545154849100949636626005868138045772194104455225660464830691777178044,89675619655127001200455492779346942960745055014995213151620690784706647509676)', 'secret_key': 7578340299196389828612605628484314963513122510741408100431794476208991022027, 'bitcoin_address': '02c1db2442b60efe9af14400d5d720f8a7043f430fe26e5e4068f51ab54783e9bc', 'private_key_dec': 193339471020636547924206028123599572328127341840628211859218753310239040869255756524643257039050969603}
+# ====================================
+# {'wif': '5Jf7dG4BUXYG2fyBZVKwifLrMuNPMHxpiH7aH9tTFLvbqVcmmQa', 'private_key_compressed_hex': '6edee963c06b3ef2758afb6a097b81f03e12cddbbefedbac9b367ff62e1a215901', 'bitcoin_address_compressed': '18VZZkEbdJHMgV3xg49YaV4rxD714ZM83U', 'bitcoin_address': '1M1E7ZEKhCa1H2V28xo8zV7Uhhi7WVH1MC', 'private_key_hex': '6edee963c06b3ef2758afb6a097b81f03e12cddbbefedbac9b367ff62e1a2159', 'private_key_dec': 50148264188737704690879791256601082022554188115509880710089775062875094262105, 'private_key_compressed_wif': 'KzwEF3T3EVnyLNzycQ2cWtEcsG9PeZsPbC81tJE6rJwaku5Xdas7', 'public_key': (32644585136304814356656023794024100879091245924523823688351220301803322373503, 100952066461685327111349222848377098017243902008859522582102727199939775015224), 'public_key_compressed_hex': '02482c2e2fbe01781597598515b4ecfbdadc8994d653c22630a4b1720b0984757f', 'public_key_hex': '04482c2e2fbe01781597598515b4ecfbdadc8994d653c22630a4b1720b0984757fdf30d8427160f41035c60f11a921c9ee72650752189640a38f0564119e91a938'}
+
+# ====================================
+
+
+  def create(self):
+    # Generate a random private key
+    valid_private_key = False
+    while not valid_private_key:
+      private_key = bitcoin.random_key()
+      decoded_private_key = bitcoin.decode_privkey(private_key, 'hex')
+      valid_private_key =  0 < decoded_private_key < bitcoin.N
+
+    # Convert private key to WIF format
+    wif_encoded_private_key = bitcoin.encode_privkey(decoded_private_key, 'wif')
+
+    # Add suffix "01" to indicate a compressed private key
+    compressed_private_key = private_key + '01'
+ 
+    # Generate a WIF format from the compressed private key (WIF-compressed)
+    wif_compressed_private_key = bitcoin.encode_privkey(bitcoin.decode_privkey(compressed_private_key, 'hex'), 'wif')
+
+    # Multiply the EC generator point G with the private key to get a public key point
+    public_key = bitcoin.fast_multiply(bitcoin.G, decoded_private_key)
+
+    # Encode as hex, prefix 04
+    hex_encoded_public_key = bitcoin.encode_pubkey(public_key,'hex')
+
+    # Compress public key, adjust prefix depending on whether y is even or odd
+    (public_key_x, public_key_y) = public_key
+
+    if (public_key_y % 2) == 0:
+        compressed_prefix = '02'
+    else:
+        compressed_prefix = '03'
+
+    hex_compressed_public_key = compressed_prefix + bitcoin.encode(public_key_x, 16)
+
+    # Generate bitcoin address from public key
+    bitcoin_address = bitcoin.pubkey_to_address(public_key) #"Bitcoin Address (b58check) is: %s\n" % 
+
+    # Generate compressed bitcoin address from compressed public key
+    bitcoin_address_compressed = bitcoin.pubkey_to_address(hex_compressed_public_key) #"Compressed Bitcoin Address (b58check) is: %s" % 
+    # return result    
+
   def test(self):
     # Generate a random private key
     valid_private_key = False
@@ -122,11 +172,3 @@ class wallet(object):
     result['bitcoin_address_compressed'] = bitcoin.pubkey_to_address(hex_compressed_public_key) #"Compressed Bitcoin Address (b58check) is: %s" % 
     return result
 
-
-# test1() vs test()
-# need to compress pub key
-# {'secret_key_58': '5HwfZDuZazMEaYRQ5va85xEtpgWkncPZ54XZKMYfJtLi6paZb55', 'public_key': '(87683569545154849100949636626005868138045772194104455225660464830691777178044,89675619655127001200455492779346942960745055014995213151620690784706647509676)', 'secret_key': 7578340299196389828612605628484314963513122510741408100431794476208991022027, 'bitcoin_address': '02c1db2442b60efe9af14400d5d720f8a7043f430fe26e5e4068f51ab54783e9bc', 'private_key_dec': 193339471020636547924206028123599572328127341840628211859218753310239040869255756524643257039050969603}
-# ====================================
-# {'wif': '5Jf7dG4BUXYG2fyBZVKwifLrMuNPMHxpiH7aH9tTFLvbqVcmmQa', 'private_key_compressed_hex': '6edee963c06b3ef2758afb6a097b81f03e12cddbbefedbac9b367ff62e1a215901', 'bitcoin_address_compressed': '18VZZkEbdJHMgV3xg49YaV4rxD714ZM83U', 'bitcoin_address': '1M1E7ZEKhCa1H2V28xo8zV7Uhhi7WVH1MC', 'private_key_hex': '6edee963c06b3ef2758afb6a097b81f03e12cddbbefedbac9b367ff62e1a2159', 'private_key_dec': 50148264188737704690879791256601082022554188115509880710089775062875094262105, 'private_key_compressed_wif': 'KzwEF3T3EVnyLNzycQ2cWtEcsG9PeZsPbC81tJE6rJwaku5Xdas7', 'public_key': (32644585136304814356656023794024100879091245924523823688351220301803322373503, 100952066461685327111349222848377098017243902008859522582102727199939775015224), 'public_key_compressed_hex': '02482c2e2fbe01781597598515b4ecfbdadc8994d653c22630a4b1720b0984757f', 'public_key_hex': '04482c2e2fbe01781597598515b4ecfbdadc8994d653c22630a4b1720b0984757fdf30d8427160f41035c60f11a921c9ee72650752189640a38f0564119e91a938'}
-
-# ====================================
